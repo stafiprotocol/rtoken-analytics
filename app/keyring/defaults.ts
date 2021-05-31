ebbd6737a94b8986792baf752fe383265828cd8a
shanzhiping <danzhiping@sina.com>
2021-05-31 21:42:03 +0800

    clean defaults.ts

--- app/keyring/defaults.ts
+++ app/keyring/defaults.ts
@@ -19,31 +19,8 @@ enum rSymbol {
   Atom = 3,
   Eth = 4,
 }
-
-
-
-const ACCOUNT_PREFIX = 'account:';
-const MAX_PASS_LEN = 32;
-
-const NONCE_LENGTH = 24;
-const PKCS8_DIVIDER = new Uint8Array([161, 35, 3, 33, 0]);
-const PKCS8_HEADER = new Uint8Array([48, 83, 2, 1, 1, 48, 5, 6, 3, 43, 101, 112, 4, 34, 4, 32]);
-const PUB_LENGTH = 32;
-
-const accountKey = (address: string): string =>
-  `${ACCOUNT_PREFIX}${address}`;
-
-const addressesKey = (symbol: string): string =>
-  `${symbol}:addresses`;
-
+ 
 export {
   Symbol,
-  rSymbol,
-  accountKey,
-  addressesKey,
-  MAX_PASS_LEN,
-  NONCE_LENGTH,
-  PKCS8_DIVIDER,
-  PKCS8_HEADER,
-  PUB_LENGTH
+  rSymbol
 };
